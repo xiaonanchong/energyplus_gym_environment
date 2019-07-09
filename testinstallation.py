3425c772dfc402af6f5a666f9cb60d780459f612
Xiaonan Chong <xc4718@edge08.doc.ic.ac.uk>
2019-07-09 14:41:20 +0100

    add testfile

--- testinstallation.py
+++ testinstallation.py
@@ -8,15 +8,14 @@ episode_length = 96 # 1day
 
 observation = env.reset()
 for i in range(episode_length):
-	action = env.action_space.sample()
-	observation, reward, done, info = env.step(action)
-
-	      
-    	plt.plot(i+1, observation[0], 'go')
-    	plt.plot(i+1, observation[1], 'co')
-    	plt.plot(i+1, observation[2], 'm+')
-    	plt.plot(i+1, observation[4]/r, 'r_')
-    	plt.plot(i+1, observation[5]/r, 'y_')
-    	plt.plot(i+1, observation[3]/r, 'k_')
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+      
+    plt.plot(i+1, observation[0], 'go')
+    plt.plot(i+1, observation[1], 'co')
+    plt.plot(i+1, observation[2], 'm+')
+    plt.plot(i+1, observation[4]/r, 'r_')
+    plt.plot(i+1, observation[5]/r, 'y_')
+    plt.plot(i+1, observation[3]/r, 'k_')
 plt.legend()
 plt.show()
