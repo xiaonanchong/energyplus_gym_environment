bdc2e1160c3dd7a59cea87b06f0daf5081b13377
xiaonanchong <xnchong@outlook.com>
2019-07-27 10:20:11 +0100

    Update gym_use.py

--- gym_use.py
+++ gym_use.py
@@ -2,10 +2,15 @@ import gym
 import gym_datacenter
 env = gym.make('gym_datacenter:datacenter-v0')  
 
-observation = env.reset()  
-print(observation)
-action =  env.action_space.sample()   
-print(action)
-observation, reward, done, info = env.step(action) 
-print(observation)
+for i in range(4):
+  observation = env.reset()  
+  print('St:')
+  print(observation)
+  action =  env.action_space.sample()  
+  print('At')
+  print(action)
+  observation, reward, done, info = env.step(action) 
+  print('St+1')
+  print(observation)
+  
 env.close()
