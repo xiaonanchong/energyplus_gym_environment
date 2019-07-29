b129b242e582719e32d598d7f9b7e0d30fc38112
xiaonanchong <xnchong@outlook.com>
2019-07-29 13:53:46 +0100

    Update gym_use.py

--- gym_use.py
+++ gym_use.py
@@ -7,7 +7,9 @@ import gym_datacenter
 env = gym.make('gym_datacenter:datacenter-v0')  
 
 power = []
-for i in range(96):
+T1 = []
+T2 = []
+for i in range(4*12):
   observation = env.reset()  
   print('St:')
   print(observation)
@@ -23,9 +25,13 @@ for i in range(96):
   
   z1 = observation[1]
   z2 = observation[2]
+  T1.append(z1)
+  T2.append(z2)
   r = 1/np.absolute(22-z1) + 1/np.absolute(22-z2)
   print('reward should be', r)
 
 plt.plot(power)
-plt.savefig('test_gym.png')
+plt.plot(T1)
+plt.plot(T2)
+plt.savefig('gym_use.png')
 #env.close()
